57a6d36011b56d1f536d1db30be1c8fb6c25cd5d
gopal das <gopal1409@gmail.com>
2020-07-29 01:13:41 -0600

    vnet configuration

--- main.tf
+++ main.tf
@@ -7,3 +7,10 @@ resource "azurerm_resource_group" "web_server_rg" {
     name = var.web_server_rg
     location = var.web_server_location
 }
+
+resource "azurerm_virtual_network" "web_server_vnet" {
+    name = "${var.resource_prefix}-vnet"
+    location = var.web_server_location
+    resource_group_name = azurerm_resource_group.web_server_rg.name
+    address_space = [var.web_server_address_space]
+}
--- variables.tf
+++ variables.tf
@@ -5,3 +5,9 @@ variable "web_server_location"{
 variable "web_server_rg"{
     type = string
 }
+variable "resource_prefix"{
+    type = string
+}
+variable "web_server_address_space"{
+    type = string
+}
