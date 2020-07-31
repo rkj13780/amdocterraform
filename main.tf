3f2235543c2945cd5d4881c4ca90952e9af5caf1
gopal das <gopal1409@gmail.com>
2020-07-31 00:13:11 -0600

    availability set

--- main.tf
+++ main.tf
@@ -72,6 +72,7 @@ resource "azurerm_windows_virtual_machine" "web_server" {
     location = var.web_server_location
     resource_group_name = azurerm_resource_group.web_server_rg.name 
     network_interface_ids = [azurerm_network_interface.web_server_nic.id]
+    availability_set_id = azurerm_availability_set.web_server_availability_set.id
     size = "Standard_B1s"
     admin_username = "webserver"
     admin_password = "Password@123456"
@@ -88,7 +89,13 @@ resource "azurerm_windows_virtual_machine" "web_server" {
         version = "latest"
     }
 }
-
+resource "azurerm_availability_set" "web_server_availability_set"{
+    name = "${var.resource_prefix}-availability-set"
+    location = var.web_server_location
+    resource_group_name = azurerm_resource_group.web_server_rg.name
+    managed = true
+    platform_fault_domain_count = 2
+}
 
 
 
