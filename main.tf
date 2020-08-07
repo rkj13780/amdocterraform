ebc4fc53eb751b146d4a13bcc5fb3f75ffb037a3
gopal das <gopal1409@gmail.com>
2020-08-07 01:42:44 -0600

    output.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,20 @@ module "location_us2w" {
     terraform_script_version = var.terraform_script_version
     admin_password = data.azurerm_key_vault_secret.admin_password.value
 }
+
+module "location_us2e" {
+    source = "./location"
+    web_server_location = "eastus2"
+    web_server_rg = "${var.web_server_rg}-us2e"
+    resource_prefix = "${var.resource_prefix}-us2e"
+    web_server_address_space = "1.0.0.0/22"
+    web_server_name = var.web_server_name
+    environment = var.environment
+    web_server_count = var.web_server_count
+    web_server_subnet = {
+        web-server = "1.0.1.0/24"
+        AzureBastionSubnet = "1.0.2.0/24"
+    }
+    terraform_script_version = var.terraform_script_version
+    admin_password = data.azurerm_key_vault_secret.admin_password.value
+}
---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "baston_host_subnet_us2w" {
+    value = module.location_us2w.baston_host_subnet
+}
+output "baston_host_subnet_us2e" {
+    value = module.location_us2e.baston_host_subnet
+}
